b5a3b77a36526f441c4c8e71857350cce89bedb5
Abhiram S R <abhiramsr173@gmail.com>
2025-08-01 18:45:46 +0530

    custom exception class created

--- exception/custom_exception.py
+++ exception/custom_exception.py
@@ -1,2 +1,25 @@
-from DOCUMENT_PORTAL
+import sys
+import traceback 
+from logs.custom_logger import CustomLogger
+logger = CustomLogger().get_logger(__file__)
+
+class DocumentPortalException(Exception):
+    def __init__(self,error_message,error_details:sys):
+        _,_,exc_tb = error_details.exc_info()
+        self.file_name = exc_tb.tb_frame.f_code.co_filename
+        self.lineno = exc_tb.tb_lineno
+        self.error_message = str(error_message)
+        self.traceback_str = ''.join(traceback.format_exception(*error_details.exc_info()))
+        
+    def __str__(self):
+        return f"""Error in [{self.file_name}] at line [{self.lineno}]
+            Messsage: {self.error_message}
+            Traceback: {self.traceback_str}"""
+            
+        
+                                        
+                                        
+                                        
+                            
+        
 
